f1555884b4e34938fa1e5dcb2c81ba2d6f0a43ef
Streetplayr1 <allenc5c5@gmail.com>
2020-05-20 06:24:13 -0600

    1.0.22

--- index.js
+++ index.js
@@ -11,10 +11,9 @@ client.on('message', message => {
         message.react(':downdoot:692862024241250334');
     }
 
-   if (message.channel.id == '638581523498532896') {
-       if (message.content === 'https://www.twitch.tv/icegamer') {
-           sleep(2000);
-           message.delete();
+   if (message.channel.id == '638581523498532896' || message.channel.id == '676135196894036000') {
+       if (message.content.find(twitch.tv/icegamer) == 1) {
+           //message.delete();
            message.channel.send("Mia, no one gives a fuck about your stream, the link is now forbidden on this server. Move along...");
        }
     }
