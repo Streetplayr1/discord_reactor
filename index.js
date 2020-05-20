0b0c6086efa19fb339cedfd0d5f611737b8328a5
Streetplayr1 <allenc5c5@gmail.com>
2020-05-20 07:05:50 -0600

    1.0.29

--- index.js
+++ index.js
@@ -12,10 +12,10 @@ client.on('message', message => {
     }
 
 
-   if (message.channel.id == '638581523498532896' || message.channel.id == '676135196894036000') {
+   /*if (message.channel.id == '638581523498532896' || message.channel.id == '676135196894036000') {
         message.delete(1000);
         message.channel.send("Mia, no one gives a fuck about your stream, the link is now forbidden on this server. Move along...");
-    }
+    }/*
 });
 
 client.login(process.env.token);
