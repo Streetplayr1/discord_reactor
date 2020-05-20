35455cb3d74e3eac50f322847d8e85a4b4450996
Streetplayr1 <allenc5c5@gmail.com>
2020-05-20 05:28:08 -0600

    1.0.8

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ client.on('ready', () => {
     console.log("Online.");
 })
 
-client.event('message', message => {
+client.on('message', message => {
     if (message.channel.id == '651494260134117376') {
         message.react(':updoot:692862052599070720');
         message.react(':downdoot:692862024241250334');
