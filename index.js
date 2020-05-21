42ea1cb907efb71752df28e3d5e9036b3010b9b1
Streetplayr1 <allenc5c5@gmail.com>
2020-05-21 00:13:00 -0600

    1.0.49

--- index.js
+++ index.js
@@ -12,9 +12,9 @@ client.on('message', message => {
     }
 
     if (message.channel.id == '638581523498532896') {
-        if (message.content.toLowerCase().includes('icegamer')) {
+        if (message.content.toLowerCase().trim().includes('icegamer')) {
             message.delete()
-                .then(message.channel.send("Mia, no one cares about your fucking stream. Message removed, move along..."));
+                .then(message.channel.send("Mia, no one cares about your fucking stream. Message removed, move along. Message self-destructs in sixty seconds."));
         }
     }
 
