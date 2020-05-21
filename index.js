3d9ba5675ef80dfe3a2bc7f2ee8d7dee3c8212d2
Streetplayr1 <allenc5c5@gmail.com>
2020-05-21 00:22:39 -0600

    1.0.51

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ client.on('message', message => {
     }
 
     if (message.channel.id == '638581523498532896') {
-        if (message.author == "goat#4491") {
+        if (message.author == "angel#5276") {
             if (message.content.toLowerCase().trim().includes('icegamer')) {
                 message.delete()
                     .then(message.channel.send("Mia, no one cares about your fucking stream. Message removed, move along. Message self-destructs in sixty seconds."));
