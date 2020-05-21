30dd1fac3c3e7309ee75eb38547e0948d01760cc
Streetplayr1 <allenc5c5@gmail.com>
2020-05-21 00:33:47 -0600

    1.0.55

--- index.js
+++ index.js
@@ -6,16 +6,19 @@ client.on('ready', () => {
 })
 
 client.on('message', message => {
+    // Version 1.0: React to meme-off posts
     if (message.channel.id == '651494260134117376') {
         message.react(':updoot:692862052599070720');
         message.react(':downdoot:692862024241250334');
     }
 
+    // Version 1.1: Mia cannot post her stream
     if (message.channel.id == '638581523498532896') {
         if (message.author.id == "216467577268862976") {
             if (message.content.toLowerCase().trim().includes('icegamer')) {
-                message.delete()
-                    .then(message.channel.send("Mia, no one cares about your fucking stream. Message removed, move along. Message self-destructs in sixty seconds."));
+                message.delete({ timeout: 1000})
+                    .then(message.channel.send("Mia, no one cares about your fucking stream. Message removed, move along. Message self-destructs in thirty seconds."))
+                    .then(msg.delete({ timeout: 30000 }));
             }
         }
     }
