770346b63bbb35d691b3ce8ca0f13d51b6510aec
Streetplayr1 <allenc5c5@gmail.com>
2020-05-21 01:32:58 -0600

    1.0.55

--- index.js
+++ index.js
@@ -6,19 +6,20 @@ client.on('ready', () => {
 })
 
 client.on('message', message => {
-    // Version 1.0: React to meme-off posts
+    // Version 1.0.0: React to meme-off posts
     if (message.channel.id == '651494260134117376') {
         message.react(':updoot:692862052599070720');
         message.react(':downdoot:692862024241250334');
     }
 
-    // Version 1.1: Mia cannot post her stream
+    // Version 1.1.0: Mia cannot post her stream in general-chat
+    var clientReply = "Mia, no one cares about your fucking stream. Message removed, move along. Message self-destructs in twenty seconds.";
     if (message.channel.id == '638581523498532896') {
         if (message.author.id == "216467577268862976") {
-            if (message.content.toLowerCase().trim().includes('icegamer')) {
+            if (message.content.toLowerCase().includes('icegamer')) {
                 message.delete({ timeout: 1500})
-                    .then(message.channel.send("Mia, no one cares about your fucking stream. Message removed, move along. Message self-destructs in twenty seconds."))
-                    .then(sentMessage.delete({ timeout: 20000 }));
+                    .then(message.channel.send(clientReply))
+                    .then(clientReply.delete({ timeout: 20000 }));
             }
         }
     }
