93b9588e1271b7ade1fdff6998993eca757e4059
Streetplayr1 <allenc5c5@gmail.com>
2020-05-25 13:18:42 -0600

    1.1.1

--- index.js
+++ index.js
@@ -8,7 +8,9 @@ client.on('ready', () => {
 client.on('message', message => {
     // Version 1.0.0: React to meme-off posts that are not pinned messages
     if (message.channel.id == '651494260134117376') {
-        if (!message.content.toLowerCase().includes('pinned a message to this channel')) {
+        if (message.content.toLowerCase().includes('to this channel')) {
+            message.channel.send("Congratulations!", { timeout: 2000});
+        } else {
             message.react(':updoot:692862052599070720');
             message.react(':downdoot:692862024241250334');
         }
